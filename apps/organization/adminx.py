15a239777af94a19b7408a03c49edf42f7c86031
anmutu <anmutu@hotmail.com>
2019-04-05 15:12:38 +0800

    1.其他apps（course,operation,organization）註冊到xadmin里。

---
+++ apps/organization/adminx.py
@@ -0,0 +1,37 @@
+# _*_ coding :utf-8 _*_
+__author__ = 'du'
+__bolg__ = 'www.cnblogs.com/anmutu;www.zmfei4.com;'
+__date__ = '2019/4/5 15:08'
+
+import xadmin
+
+from .models import CityDict, CourseOrg, Teacher
+
+
+class CityDictAdmin(object):
+    """城市"""
+
+    list_display = ['name', 'desc', 'add_time']
+    search_fields = ['name', 'desc']
+    list_filter = ['name', 'desc', 'add_time']
+
+
+class CourseOrgAdmin(object):
+    """机构"""
+
+    list_display = ['name', 'desc', 'click_nums', 'fav_nums', 'add_time']
+    search_fields = ['name', 'desc', 'click_nums', 'fav_nums']
+    list_filter = ['name', 'desc', 'click_nums', 'fav_nums', 'city__name', 'address', 'add_time']
+
+
+class TeacherAdmin(object):
+    """老师"""
+
+    list_display = ['name', 'org', 'work_years', 'work_company', 'add_time']
+    search_fields = ['org', 'name', 'work_years', 'work_company']
+    list_filter = ['org__name', 'name', 'work_years', 'work_company', 'click_nums', 'fav_nums', 'add_time']
+
+
+xadmin.site.register(CityDict, CityDictAdmin)
+xadmin.site.register(CourseOrg, CourseOrgAdmin)
+xadmin.site.register(Teacher, TeacherAdmin)
